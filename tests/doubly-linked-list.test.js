ea176a260cb7b28c108ed65d1e7d771145692644
Francis Ngo <francis.t.ngo@gmail.com>
2023-03-18 00:48:31 -0700

    Add doubly linked list tests

--- tests/doubly-linked-list.test.js
+++ tests/doubly-linked-list.test.js
@@ -98,25 +98,25 @@ describe('DoublyLinkedList', () => {
         verifyList();
     });
 
-    it('inserts elements invalid position empty list', () => {
+    test('inserts elements invalid position empty list', () => {
         expect(list.insert(1, 1)).toEqual(false);
     });
 
-    it('inserts elements at the end of the list', () => {
+    test('inserts elements at the end of the list', () => {
         max = 5;
         for(let i = min; i <= max; i++) {
             expect(list.insert(i, i - 1)).toEqual(true);
         }
     });
 
-    it('inserts elements in the middle of list', () => {
+    test('inserts elements in the middle of list', () => {
         expect(list.insert(3, 0)).toEqual(true);
         expect(list.insert(1, 0)).toEqual(true);
         expect(list.insert(2, 1)).toEqual(true);
         verifyList();
     });
 
-    it('returns index of elements', () => {
+    test('returns index of elements', () => {
         let index;
         pushesElements();
         for(let i = min; i <= max; i++) {
@@ -125,7 +125,7 @@ describe('DoublyLinkedList', () => {
         }
     });
 
-    it('removes invalid elements', () => {
+    test('removes invalid elements', () => {
         let element;
         pushesElements();
         for (let i = max + 2; i <= max + 4; i++) {
@@ -134,13 +134,100 @@ describe('DoublyLinkedList', () => {
         }
     });
 
-    it('removes valid elements', () => {
+    test('removes valid elements', () => {
         let element;
         pushesElements();
         for (let i = min; i <= max; i++) {
             element = list.remove(i);
             expect(element).toBeDefined();
             expect(element).toEqual(i);
+        }
+    });
+
+    test('removes element invalid position empty list', () => {
+        let element; 
+        for (let i = min; i <= max; i++) {
+            element = list.removeAt(i - 1);
+            expect(element).toBeUndefined();
+        }
+    });
+
+    test('removes element invalid position not empty list', () => {
+        let element;
+        pushesElements();
+        for(let i = max + 2; i <= max + 4; i++) {
+            element = list.removeAt(i);
+            expect(element).toBeUndefined();
+        }
+    });
+
+    test('removes first element list single element', () => {
+        const value = 1;
+        list.push(value);
+
+        const element = list.removeAt(0);
+        expect(element).toBeDefined();
+        expect(element).toEqual(value);
+
+        expect(list.getHead()).toBeUndefined();
+        expect(list.getTail()).toBeUndefined();
+        expect(list.isEmpty()).toEqual(true);
+    });
+
+    test('removes first element list multiple elements', () => {
+        pushesElements();
+        const element = list.removeAt(0);
+        expect(element).toBeDefined();
+        expect(element).toEqual(min);
+        min = 2;
+        verifyList();
+    });
+
+    test('removes element from end of list', () => {
+        let element;
+        pushesElements();
+        const maxIndex = max;
+        for(let i = maxIndex; i >= min; i--) {
+            element = list.removeAt(i - 1);
+            expect(element).toBeDefined();
+            expect(element).toEqual(i);
+            max--;
+            verifyList();
+        }
+    });
+
+    it('removes elements from middle of list', () => {
+        pushesElements(); 
+        const element = list.removeAt(1); // element 2
+        expect(element).toBeDefined();
+        expect(element).toEqual(2);
+
+        // list should be [1, 3]
+        let current = list.getHead();
+
+        // element 1
+        expect(current).toBeDefined();
+        if (current) {
+            expect(current.element).toBeDefined();
+            expect(current.element).toEqual(1);
+            expect(current.prev).toBeUndefined();
+            expect(current.next).toBeDefined();
+            if (current.next) {
+                expect(current.next.element).toEqual(3);
+                current = current.next;
+            }
+        }
+
+        // element 3
+        expect(current).toBeDefined();
+        if (current) {
+            expect(current.element).toBeDefined();
+            expect(current.element).toEqual(3);
+            expect(current.next).toBeUndefined();
+            expect(current.prev).toBeDefined();
+            if (current.prev) {
+                expect(current.prev.element).toEqual(1);
+            }
         }
     });
 });
