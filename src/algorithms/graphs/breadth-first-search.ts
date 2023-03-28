e7d780942251129b0791700d32320decbda73c35
Francis Ngo <francis.t.ngo@gmail.com>
2023-03-28 07:53:07 -0700

    Add bfs graph notes

--- src/algorithms/graphs/breadth-first-search.ts
+++ src/algorithms/graphs/breadth-first-search.ts
@@ -2,14 +2,21 @@ import Graph from "../../data-structures/graph";
 import Queue from "../../data-structures/queue";
 import { Colors, initializeColor } from "../../utils";
 
+// The BFS algorithm starts traversing the graph from the first specified vertex and visits all its neighbors (adjacent vertices) first, one layer of the graph at a time. In other words, it visits the vertices first widely and then deeply
 export const breadthFirstSearch = (graph: Graph, startVertex: any, callback: Function) => {
     const vertices = graph.getVertices();
     const adjList = graph.getAdjList();
+    // initialize all vertices to be white (not visited);
     const color = initializeColor(vertices);
+    // create a queue to store vertices that need to be visited
     const queue = new Queue();
 
+    // enqueue the given vertex
     queue.enqueue(startVertex);
-
+    
+    // remove a vertex from queue 
+    // fetch adjacency list that contains all it's neighbors
+    // and mark them as grey (discovered but not visited yet)
     while(!queue.isEmpty()) {
         const u = queue.dequeue();
         const neighbors = adjList.get(u);
@@ -18,17 +25,21 @@ export const breadthFirstSearch = (graph: Graph, startVertex: any, callback: Fun
         if (neighbors !== undefined) {
             for (let i = 0; i < neighbors.length; i++) {
                 const w = neighbors[i];
+                // if there are any neighbor vertices that are not yet discovered, mark them as grey and add them to enqueue
                 if (color[w] === Colors.WHITE) {
                     color[w] = Colors.GREY;
                     queue.enqueue(w);
                 }
             }
+            // mark them as visited
+            // and use callback to do something with the vertices
             color[u] = Colors.BLACK;
             if (callback) callback(u);
         }
     }
 }
 
+// method to find the shortest path(s)
 export const BFS = (graph: Graph, startVertex: any) => {
     const vertices = graph.getVertices();
     const adjList = graph.getAdjList();
@@ -52,7 +63,9 @@ export const BFS = (graph: Graph, startVertex: any) => {
                 const w = neighbors[i];
                 if (color[w] === Colors.WHITE) {
                     color[w] = Colors.GREY;
+                    // distance from v to u
                     distances[w] = distances[u] + 1;
+                    // derive a shortest path from v to every other vertex u
                     predecessors[w] = u;
                     queue.enqueue(w);
                 }
@@ -62,7 +75,7 @@ export const BFS = (graph: Graph, startVertex: any) => {
     }
   
     return {
-        distances: distances,
-        predecessors: predecessors
+        distances,
+        predecessors
     };
 }
